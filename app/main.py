361f0e1e05fe6f76f56a3819eb1449273613c776
Noobosapien <migirulz44@gmail.com>
2024-08-31 00:15:35 +1200

    CORS policies

--- app/main.py
+++ app/main.py
@@ -3,12 +3,25 @@ import logging
 import logging.config
 from app.routers import storyboard_routes
 from app.routers import test_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(
     storyboard_routes.router, prefix="/api/storyboard", tags=["Storyboard"]
 )
